5ac0072d03c9dad123afe678f3cf3bdc155c7ae5
Monopolise <shgu9511@uni.sydney.edu.au>
2024-10-22 21:25:20 +1100

    admin_register_model

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserData, Track, Artist
 
+admin.site.register(UserData)
+admin.site.register(Track)
+admin.site.register(Artist)
 # Register your models here.
