c77d798051cdcc516687046e95d4101a10903d03
TianruiZhang <tzha6323@uni.sydney.edu.au>
2024-10-25 01:25:01 +1100

    Revert "qq"
    This reverts commit 44f531aa8e2ca1600a392169c8fc3d02adbb55ef, reversing
    changes made to 59f6e6641adc2737075bfd9cfef5dff6bd1bcb9c.

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,35 +1,7 @@
 from django.urls import path
-
-from myapp.views.auth_views import (
-    login_view,
-    register_view,
-    logout_view,
-    session_view,
-    get_csrf_token
-)
-
-from myapp.views.create_object_views import (
-    create_playlist,
-    add_track,
-    add_album,
-    add_artist,
-    add_genre
-)
-
-from myapp.views.data_request_views import (
-    get_album_info,
-    get_music
-)
-
-from myapp.views.linking_views import (
-    add_track_to_playlist,
-    share_playlist,
-    link_track_to_artist,
-    link_track_to_album,
-    link_track_to_genre,
-    link_album_to_artist,
-    remove_album_track_link
-)
+from .views import (login_view, logout_view, session_view, get_csrf_token, register_view, create_playlist, create_track, add_track_to_playlist,
+                    share_playlist, make_album, make_genre, get_album_info, get_music, search_songs
+                    )
 
 urlpatterns = [
     path('login/', login_view, name='login'),
@@ -38,15 +10,13 @@ urlpatterns = [
     path('get_csrf_token/', get_csrf_token, name='csrf'),
     path('register/', register_view, name='register'),
     path('create_playlist/', create_playlist, name='create_playlist'),
+    path('create_track/', create_track, name='create_track'),
     path('add_track_to_playlist/', add_track_to_playlist, name='add_track_to_playlist'),
     path('share_playlist/', share_playlist, name='share_playlist'),
+    path('make_album/', make_album, name='make_album'),
+    path('make_genre/', make_genre, name='make_genre'),
     path('get_album/', get_album_info, name='get_album_info'),
     path('get_music/', get_music, name='get_music'),
-    path('add_track/', add_track, name='add_track'),
-    path('add_album/', add_album, name='add_album'),
-    path('add_artist/', add_artist, name='add_artist'),
-    path('add_genre/', add_genre, name='add_genre'),
-    path('link_track_to_album/', link_track_to_album, name='link_track_to_album'),
-    path('link_album_to_artist/', link_album_to_artist, name='link_album_to_artist'),
-    path('link_track_to_artist/', link_track_to_artist, name='link_track_to_artist'),
+    path('search_songs/', search_songs, name='search_songs')
+
 ]
